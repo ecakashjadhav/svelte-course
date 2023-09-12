7e6028df092ae9fcfd31c41139cf9149e4268125
ecakashjadhav <akash.jadhav@ecinfosolutions.com>
2023-09-12 15:12:57 +0530

    bind input value to the variable

---
+++ src/lib/TodoList.svelte
@@ -0,0 +1,34 @@
+<script>
+	import Button from "./Button.svelte";
+
+    export let todos = [];
+    // let input;
+    let inputText = "";
+
+    function handleAddTodo() {
+        console.log(inputText);
+    }
+</script>
+
+<div class="todo-list-wrapper">
+
+    <ul>
+        <!-- {#each todos as todo, index} -->
+        {#each todos as {id,title}, index(id)}
+            {@const number = index + 1}
+            <li>{number} {title}</li>
+            <!-- <li>{index + 1} {todo.title}</li> -->
+        {/each}
+    </ul>
+    {inputText}
+    <form class="add-todo-form" on:submit|preventDefault = {handleAddTodo}>
+        <!-- <input bind:this={input}/> -->
+        <!-- <input on:input={(e) => {
+            inputText = e.currentTarget.value;
+            // console.log(e.currentTarget.value);
+        }}/> -->
+        <input bind:value={inputText} />
+        <Button type="submit">Add</Button>  
+    </form>
+
+</div>
