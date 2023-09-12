e5f3dbb4c6d2398044d1fe8b25941ae3ced246bb
ecakashjadhav <akash.jadhav@ecinfosolutions.com>
2023-09-12 13:56:21 +0530

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+	import Button from './lib/Button.svelte';
+	import FaAngellist from 'svelte-icons/fa/FaAngellist.svelte';
+	import FaAllergies from 'svelte-icons/fa/FaAllergies.svelte';
+</script>
+
+<Button size="small" shadow>
+	<div let:isLeftHovered style:width="20px" slot="leftContent">
+		{#if isLeftHovered}
+			<FaAngellist />
+		{:else}
+			<FaAllergies />
+		{/if}
+	</div>
+	Button Text
+</Button>
+
+<style>
+</style>
---
+++ src/lib/Counter.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let initialCount;
+  export let maxCount;
+  let count = initialCount;
+
+  function increment() {
+    if (count === maxCount) return;
+    count += 1;
+  }
+</script>
+
+
+<button type="button" on:click={increment}>Click {count}</button>
